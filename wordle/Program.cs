66727fd6322e5e3a106025a1b4e9df10e8381092
mehmetiyiyazici <164754796+mehmetiyiyazici@users.noreply.github.com>
2024-04-27 01:37:43 +0300

    Add files via upload

---
+++ wordle/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>
+        {
+            "kuyumcu", "araba", "kamera", "kalem", "kedi", "karpuz", "kırmızı", "kıyafet", "kütüphane", "kış"
+        };
+         
+
+        Console.WriteLine("Oyuna Başla");
+        string cevap = list[new Random().Next(list.Count)];
+        Console.WriteLine("1-Başlamak İçin");
+        string girilenDeger = Console.ReadLine();
+        int girilenSayi = int.Parse(girilenDeger);
+        Console.Clear();
+        if (girilenSayi == 1)
+        {
+            string tahmin = " ";
+            
+            while (tahmin != cevap)
+            {
+                Console.WriteLine("\nTahmin Girin");
+                tahmin = Console.ReadLine();
+                string sonuc = "";
+                for (int i = 0; i < tahmin.Length; i++)
+                {
+                    
+                    if (cevap.Contains(tahmin[i]))
+                    {
+                        if (cevap[i] == tahmin[i])
+                        {
+                            sonuc += $"\u001b[32m{tahmin[i]}\u001b[0m"; 
+                        }
+                        else
+                        {
+                            sonuc += $"\u001b[33m{tahmin[i]}\u001b[0m"; 
+                        }
+                    }
+                    else
+                    {
+                        sonuc += "_";
+                    }
+
+                }
+                Console.WriteLine(sonuc);
+                
+            } 
+        }
+    }
+}
